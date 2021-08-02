b7faa3946c5609db0766bec99118cf72181dd2e9
Tony <gth1123@naver.com>
2021-08-02 12:44:19 +0900

    useEffect test

--- slider/README.md
+++ slider/README.md
@@ -7,3 +7,8 @@
 ## hammer
 
 ## swipeable-views
+
+## useEffect test
+
+- 처음 1회는 무조건 실행, dependencies array 안에 있는 것이 바뀔 때 마다 다시 실행
+- useEffect안에서 useState의 setState로 값을 변경 하면 즉시 반영 되지 않고 비동기로 내려간 다음 렌더링할 땐 setState로 변경된 값이 반영됨
--- slider/src/pages/Home.js
+++ slider/src/pages/Home.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AppLayout from '../components/AppLayout';
 import { MainSlider } from '../components/Slider';
 // import PauseOnHover from '../components/PauseOnHover';
 
 const Home = () => {
+  const [text, setText] = useState('asdf');
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log(text);
+    setText('qwer');
+    console.log(text);
+    console.log(count);
+  }, [count]);
+  const onClickBtn = () => {
+    setCount((count) => (count = count + 1));
+    // console.log('count : ', count);
+  };
   return (
     <AppLayout>
+      {console.log('rendering text', text)}
+      {/* {console.log('count : ', count)} */}
       <MainSlider />
+      <button onClick={onClickBtn}>click {count}</button>
     </AppLayout>
   );
 };
