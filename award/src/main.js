53cd7ab18d35d794e887c85f707acc85f8aab592
TaehwanGo <gth1123@naver.com>
2021-08-09 20:56:50 +0900

    keyup 잘되는데? 아깐 왜 안된거지?

--- award/src/main.js
+++ award/src/main.js
@@ -1,12 +1,14 @@
 // import './test';
-import { testFunction } from './test';
+import { testFunction } from './test.js';
 
 const mainList = document.querySelector('.awardList');
+const inputTest = document.querySelector('.input__test');
+const esInput = document.querySelector('#esInput');
 
 // dummyData
 const dummyMainListData = Array(20)
   .fill()
-  .map((v) => ({
+  .map(v => ({
     itemLink: '/AwardFestival/WinnersList.asp',
     itemImg:
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbjjOfEKwT2cspVrDjuJElO5QPOY_7C-xOTA&usqp=CAU',
@@ -16,35 +18,44 @@ const dummyMainListData = Array(20)
     region: 'Lisbon, Portugal',
   }));
 
-console.log(dummyMainListData);
+// console.log(dummyMainListData);
 let count = 0;
 
 // MainList
-const AddMainList = (mainListData) => {
-  mainListData.forEach((mainListItem) => {
-    console.log(count++);
-    mainList.innerHTML += String.raw`<li>
-    <a href=${mainListItem.url} class="linkItem">
-      <div class="imgWrap">
-        <div class="imgCell">
-          <img src=${mainListItem.itemImg} alt=${mainListItem.title} />
-        </div>
-        <div class="imgCell small">
-          <img src=${mainListItem.itemImgSmall} alt=${mainListItem.title} />
-        </div>
-      </div>
-      <div class="txtWrap">
-        <p class="txt txt01">${mainListItem.title}</p>
-        <p class="txt txt02">${mainListItem.region}</p>
-      </div>
-    </a>
-  </li>`;
-  });
-};
+// const AddMainList = mainListData => {
+//   mainListData.forEach(mainListItem => {
+//     console.log(count++);
+//     mainList.innerHTML += String.raw`<li>
+//     <a href=${mainListItem.url} class="linkItem">
+//       <div class="imgWrap">
+//         <div class="imgCell">
+//           <img src=${mainListItem.itemImg} alt=${mainListItem.title} />
+//         </div>
+//         <div class="imgCell small">
+//           <img src=${mainListItem.itemImgSmall} alt=${mainListItem.title} />
+//         </div>
+//       </div>
+//       <div class="txtWrap">
+//         <p class="txt txt01">${mainListItem.title}</p>
+//         <p class="txt txt02">${mainListItem.region}</p>
+//       </div>
+//     </a>
+//   </li>`;
+//   });
+// };
+function onKeyup(event) {
+  if (event.keyCode === 13) {
+    console.log('enter');
+  }
+}
 
 function init() {
-  AddMainList(dummyMainListData);
+  // AddMainList(dummyMainListData);
   testFunction();
+  inputTest.addEventListener('keyup', () => {
+    console.log('input');
+  });
+  esInput.addEventListener('keyup', onKeyup);
 }
 
 init();
