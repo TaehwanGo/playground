ea5ca66e16f1622632960dc8d7ca32c6b20d320a
TaehwanGo <gth1123@bubblecon.io>
2023-09-12 17:38:38 +0900

    flip card on click

---
+++ animation/flip-card/main.js
@@ -0,0 +1,5 @@
+const card = document.querySelector(".flip");
+
+card.addEventListener("click", function () {
+  card.classList.toggle("is-flipped");
+});
---
+++ animation/flip-card/readme.md
@@ -0,0 +1,3 @@
+# 카드 뒤집기
+- 참고
+  - https://gurtn.tistory.com/157
